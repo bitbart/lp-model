c02a030834827567f5568017f33e52d450d03dae
Massimo Bartoletti <bart@unica.it>
2025-04-03 09:11:38 +0200

    added README.md + moved string_utils.py

---
+++ README.md
@@ -0,0 +1,30 @@
+# Lending Protocols simulator
+
+## Prerequisites
+
+For unit tests with pytest:
+```bash
+pip3 install -U pytest
+```
+
+For property-based testing with Hypothesis:
+```bash
+pip3 install hypothesis
+```
+
+## Usage
+
+To execute a sequence of transactions:
+```bash
+python lp.py trace1.txt
+```
+
+To execute unit tests:
+```bash
+pytest test_lp.py
+```
+
+To execute property-based tests:
+```bash
+pytest pbt_lp.py
+```
--- lp.py
+++ lp.py
@@ -1,6 +1,7 @@
 import math # For math.inf
 from math import isclose
 from fractions import Fraction
+from string_utils import *
 
 # from hypothesis import note, assume, settings
 # from hypothesis.strategies import *
@@ -24,36 +25,6 @@ Constants = namedtuple('Constants', ['Cmin','Rliq'])
 LP_constants = Constants(1.5, 1.1)
 
 from fractions import Fraction
-
-def clean_repr(obj, precise=False):
-     """
-     Recursively converts objects to a string representation
-     with custom formatting for dictionaries, lists, and fractions.
-     """
-     if isinstance(obj, dict):
-         return "{" + ", ".join(f"{k}: {clean_repr(v)}" for k, v in obj.items()) + "}"
-     elif isinstance(obj, list):
-         return "[" + ", ".join(clean_repr(v) for v in obj) + "]"
-     elif isinstance(obj, Fraction):
-         return f"{obj.numerator}/{obj.denominator}" if precise else format_float(float(obj))
-     elif isinstance(obj, float):
-         return format_float(obj)
-     elif isinstance(obj, str):
-         return obj  # Return strings without quotes
-     else:
-         return repr(obj)  # Default representation for other types
-
-def format_float(value):
-    """
-    Format a floating-point number cleanly:
-    - Remove unnecessary decimals and trailing zeros.
-    - Keep precision up to meaningful digits.
-    """
-    if value.is_integer():
-        return f"{int(value)}"  # Represent as an integer if no fractional part
-    else:
-        return f"{value:.6g}"  # Use general format with up to 6 significant digits
-
 
 class LP(RuleBasedStateMachine):
     """
